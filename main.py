22139774cfca56b2f3cf9e8eaef4f281158477ae
speherh <spencer@speherh.com>
2017-05-16 15:23:04 -0400

    Add cringy nouns and whatever

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import random
-//code goes here
+
+
+noun1 = [ 'star', 'keyboard', 'electricity', 'calculator', 'asian', 'hersheybar', 'youtube', 'sister', 'politician', 'smallchild' ]
+body1 = [ 'face', 'elbow', 'tooth', 'arm', 'navel', 'throat', 'knife', 'eye', 'regret', 'toenail', 'asian' ]
+noun2 = [ 'paper', 'skin', 'asian', 'number', 'lcd', 'lsd', 'profile', 'regret', 'mistake' ]
+anim1 = [ 'cat', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'asian', 'starfish', 'waterbear' ]
