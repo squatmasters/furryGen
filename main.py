ec35730ec7afe7eca02b4597759dfc127178ce83
moistboi3000 <moistboi3000@gmail.com>
2017-05-17 10:15:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-User_name = raw_input("What is your name, my throbbing friend?")
+raw_input("What is your name, my throbbing friend?")
 
 noun1 = [ 'star', 'keyboard', 'electricity', 'calculator', 'asian', 'hersheybar', 'youtube', 'sister', 'politician', 'smallchild' ]
 body1 = [ 'face', 'elbow', 'tooth', 'arm', 'navel', 'throat', 'knife', 'eye', 'regret', 'toenail', 'asian' ]
