4449463d8af354a6c0428ece207264b3e6bcf65a
moistboi3000 <moistboi3000@gmail.com>
2017-05-17 10:47:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ noun1 = [ 'star', 'keyboard', 'electricity', 'calculator', 'asian', 'hersheybar'
 body1 = [ 'face', 'elbow', 'tooth', 'arm', 'navel', 'throat', 'knife', 'eye', 'regret', 'toenail', 'asian' ]
 noun2 = [ 'paper', 'skin', 'asian', 'number', 'lcd', 'lsd', 'profile', 'regret', 'mistake' ]
 anim1 = [ 'cat', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'beaver', 'asian', 'starfish', 'waterbear' ]
+
+io = random.randint(0, len(noun1)-1)
+ip = random.randint(0, len(body1)-1)
+il = random.randint(0, len(noun2)-1)
+ik = random.randint(0, len(anim1)-1)
+
+
+
+
+
+print("you're furry name is" , User_name, "the", noun1[io] + body1[ip] , noun2[il] + anim1[ik])
